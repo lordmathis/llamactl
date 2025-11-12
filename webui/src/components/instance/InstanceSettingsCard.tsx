c022746cd8c24f780553ad3553126c15d8b7b7c3
LordMathis <matus@namesny.com>
2025-11-12 23:13:09 +0100

    Fix import path for EnvVarsInput component

--- webui/src/components/instance/InstanceSettingsCard.tsx
+++ webui/src/components/instance/InstanceSettingsCard.tsx
@@ -6,7 +6,7 @@ import { Input } from '@/components/ui/input'
 import AutoRestartConfiguration from '@/components/instance/AutoRestartConfiguration'
 import NumberInput from '@/components/form/NumberInput'
 import CheckboxInput from '@/components/form/CheckboxInput'
-import EnvVarsInput from '@/components/form/EnvironmentVariablesInput'
+import EnvVarsInput from '@/components/form/EnvVarsInput'
 import SelectInput from '@/components/form/SelectInput'
 import { nodesApi, type NodesMap } from '@/lib/api'
 
